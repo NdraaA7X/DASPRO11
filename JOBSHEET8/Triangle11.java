3c1615c4e2ab56c4f5ce41ac89acb2be5acb6607
Hendra <erhanhendragianpermana@gmail.com>
2025-10-30 11:49:09 +0700

    DASPRO

---
+++ JOBSHEET8/Triangle11.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Triangle11 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukan Nilai N = ");
+        int n = sc.nextInt();
+        int i = 1;
+        while (i <= n ) {
+            int j = 0;
+            while (j < i) {
+                System.out.print(" * ");
+                j++;
+            }
+            System.out.println();
+            i++;
+        }
+        sc.close();
+    }
+}
