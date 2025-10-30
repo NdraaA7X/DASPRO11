b9a04d580a05494b2e8a5cfe1e563ffcd335668c
Hendra <erhanhendragianpermana@gmail.com>
2025-10-30 19:15:26 +0700

    DASPRO

---
+++ JOBSHEET#6/SistemKelulusan11.java
@@ -0,0 +1,18 @@
+package Jobsheet6;
+
+import java.util.Scanner;
+public class SistemKelulusan11 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("==== INPUT DATA MAHASISWA ====");
+        System.out.println("Masukan Nama Mahasiswa: ");
+        double nama = sc.nextDouble();
+        System.out.println("Masukan NIM Mahasiswa: ");
+        double nim = sc.nextDouble();
+
+
+        
+    }
+    
+}
