68bfbcb911b77a5c9bab6b2531c1ca5b74dca75e
Hendra <erhanhendragianpermana@gmail.com>
2025-11-29 11:54:05 +0700

    DASPRO

--- JOBSHEET#2/StudiKasus2Modifikasi_11.java
+++ JOBSHEET#2/StudiKasus2Modifikasi_11.java
@@ -5,7 +5,7 @@ public class StudiKasus2Modifikasi_11 {
     public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
 
-    double lebar_tanah = 50, panjang_tanah= 100, jumlah_kolam = 2, diameter = 2, sisi = 2;
+    double lebar_tanah = 50, panjang_tanah= 100, diameter = 2, sisi = 2;
     double luas_kolam_persegi, phi = 3.14,luas_kolam_lingkaran, luas_tanah, sisa_tanah;
 
     System.out.println("Panjang Tanah : ");
@@ -19,7 +19,7 @@ public class StudiKasus2Modifikasi_11 {
     System.out.println("Panjang sisi Kolam Persegi : ");
     sisi = sc.nextInt();
     System.out.println("Kolam Lingkaran ? ");
-    jumlah_kolam = sc.nextInt();
+    double jumlah_kolam = sc.nextInt();
     System.out.println("Kolam Persegi ?");
     jumlah_kolam = sc.nextInt();
 
