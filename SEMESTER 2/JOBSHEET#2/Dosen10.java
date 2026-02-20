ec2ff67aa174c88bc0723b1b5c45dd05474b28b8
Hendra <erhanhendragianpermana@gmail.com>
2026-02-20 20:31:49 +0700

    Praktikum

---
+++ SEMESTER 2/JOBSHEET#2/Dosen10.java
@@ -0,0 +1,35 @@
+public class Dosen10 {
+    
+    String idDosen;
+    String nama;
+    boolean statusAktif;
+    int tahunBergabung;
+    String bidangKeahlian;
+
+    public Dosen10() {
+    }
+
+    void tampilInformasi() {
+        System.out.println("===== PROFIL DOSEN =====");
+        System.out.println("ID Dosen     : " + idDosen);
+        System.out.println("Nama Dosen   : " + nama);
+        System.out.println("Status Dosen : " + (statusAktif ? "Aktif" : "Nonaktif"));
+        System.out.println("Tahun Gabung : " + tahunBergabung);
+        System.out.println("Keahlian     : " + bidangKeahlian);
+        System.out.println("========================");
+    }
+
+     void setStatusAktif(boolean status) {
+        statusAktif = status;
+    }
+    
+    int hitungMasaKerja(int tahunSekarang) {
+        int masa = tahunSekarang - tahunBergabung;
+        return masa;
+    }
+
+    void ubahKeahlian(String keahlianBaru) {
+        bidangKeahlian = keahlianBaru;
+    }
+    
+}
---
+++ SEMESTER 2/JOBSHEET#2/DosenMain10.java
@@ -0,0 +1,18 @@
+public class DosenMain10 {
+    
+    public static void main(String[] args) {
+        Dosen10 dosen10 = new Dosen10();
+        dosen10.idDosen = "D001";
+        dosen10.nama = "Budi";
+        dosen10.statusAktif = true;
+        dosen10.tahunBergabung = 2015;
+        dosen10.bidangKeahlian = "Pemrograman";
+
+        dosen10.tampilInformasi();
+        System.out.println("Masa Kerja: " + dosen10.hitungMasaKerja(2026) + " tahun");
+
+        dosen10.ubahKeahlian("Jaringan");
+        System.out.println("Setelah ubah keahlian");
+        dosen10.tampilInformasi();
+    }
+}
